17eb72050d16dd1ccdb7e5b60b87eca168676a9e
nargiz <nargiztologonovna@gmail.com>
2023-08-03 13:19:02 +0600

    Второе задание первая часть.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const { encrypt } = require('vigenere-cipher');
 const app = express();
 const port = 8000;
+const password = 'password';
 
 app.get('/:index', (req, res) => {
     res.send(`<h1>${req.params.index.toUpperCase()}</h1>`);
 });
+
+app.get('/encode/:id', (req, res) =>{
+    const cipherText = encrypt(req.params.id, password);
+    res.send(`${cipherText.toUpperCase()}`);
+});
+
 
 app.listen(port, () => {
     console.log(`Server running at ${port} port!`)
